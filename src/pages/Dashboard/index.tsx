fd09cb65ad192801c9fce9edb65447a9801a7755
Marcel Pinto <p.marcel.augusto@gmail.com>
2020-07-04 20:08:06 -0300

    Conectado com a api do github

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,57 +1,68 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
+
 import logo from '../../assets/logo.svg';
 import { Title, Form, Repositories } from './style';
 
-const Dashboard: React.FC = () => (
-  <>
-    <img src={logo} alt="Github Explorer" />
-    <Title>Explore Repositórios no Github</Title>
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  }
+}
 
-    <Form>
-      <input placeholder="Digite o nome do repositório" />
-      <button type="submit">Pesquisar</button>
-    </Form>
+const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
-    <Repositories>
-      <a href="teste">
-        <img
-          src="https://avatars1.githubusercontent.com/u/46410965?s=460&u=a4612df5445069a8a2df2efce9183d822d78e18f&v=4"
-          alt="Avatar"
+  async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const respository = response.data;
+
+    setRepositories([...repositories, respository]);
+    setNewRepo('');
+  }
+
+  return (
+    <>
+      <img src={logo} alt="Github Explorer" />
+      <Title>Explore Repositórios no Github</Title>
+
+      <Form onSubmit={handleAddRepository}>
+        <input
+          placeholder="Digite o nome do repositório"
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
         />
-        <div>
-          <strong>password-checker</strong>
-          <p> A simple python script that uses a pwnedpassword api to check if a password has been leaked.</p>
-        </div>
-        <FiChevronRight size={20} />
-      </a>
+        <button type="submit">Pesquisar</button>
+      </Form>
 
-      <a href="teste">
-        <img
-          src="https://avatars1.githubusercontent.com/u/46410965?s=460&u=a4612df5445069a8a2df2efce9183d822d78e18f&v=4"
-          alt="Avatar"
-        />
-        <div>
-          <strong>password-checker</strong>
-          <p> A simple python script that uses a pwnedpassword api to check if a password has been leaked.</p>
-        </div>
-        <FiChevronRight size={20} />
-      </a>
-
-      <a href="teste">
-        <img
-          src="https://avatars1.githubusercontent.com/u/46410965?s=460&u=a4612df5445069a8a2df2efce9183d822d78e18f&v=4"
-          alt="Avatar"
-        />
-        <div>
-          <strong>password-checker</strong>
-          <p> A simple python script that uses a pwnedpassword api to check if a password has been leaked.</p>
-        </div>
-        <FiChevronRight size={20} />
-      </a>
-    </Repositories>
-  </>
-);
+      <Repositories>
+        {repositories.map((repository) => (
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>
+                {repository.full_name}
+              </strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
+      </Repositories>
+    </>
+  );
+};
 
 export default Dashboard;
