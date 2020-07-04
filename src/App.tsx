a9e8db992bcce41390305ea4e5c63d2bf13243f5
Marcel Pinto <p.marcel.augusto@gmail.com>
2020-07-04 19:27:38 -0300

    Criado a interface da aplicacao

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import Routes from './routes';
+import GlobalStyle from './style/global';
 
 const App: React.FC = () => (
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>
+  <>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+    <GlobalStyle />
+  </>
 );
 
 export default App;
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,57 @@
 import React from 'react';
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>;
+import { FiChevronRight } from 'react-icons/fi';
+import logo from '../../assets/logo.svg';
+import { Title, Form, Repositories } from './style';
+
+const Dashboard: React.FC = () => (
+  <>
+    <img src={logo} alt="Github Explorer" />
+    <Title>Explore Repositórios no Github</Title>
+
+    <Form>
+      <input placeholder="Digite o nome do repositório" />
+      <button type="submit">Pesquisar</button>
+    </Form>
+
+    <Repositories>
+      <a href="teste">
+        <img
+          src="https://avatars1.githubusercontent.com/u/46410965?s=460&u=a4612df5445069a8a2df2efce9183d822d78e18f&v=4"
+          alt="Avatar"
+        />
+        <div>
+          <strong>password-checker</strong>
+          <p> A simple python script that uses a pwnedpassword api to check if a password has been leaked.</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+
+      <a href="teste">
+        <img
+          src="https://avatars1.githubusercontent.com/u/46410965?s=460&u=a4612df5445069a8a2df2efce9183d822d78e18f&v=4"
+          alt="Avatar"
+        />
+        <div>
+          <strong>password-checker</strong>
+          <p> A simple python script that uses a pwnedpassword api to check if a password has been leaked.</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+
+      <a href="teste">
+        <img
+          src="https://avatars1.githubusercontent.com/u/46410965?s=460&u=a4612df5445069a8a2df2efce9183d822d78e18f&v=4"
+          alt="Avatar"
+        />
+        <div>
+          <strong>password-checker</strong>
+          <p> A simple python script that uses a pwnedpassword api to check if a password has been leaked.</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+    </Repositories>
+  </>
+);
 
 export default Dashboard;
